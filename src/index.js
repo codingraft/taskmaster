9defe0d20ae7cea78843bbbe6be9200f697f9f2a
Vaibhavg987 <78644114+Vaibhavg987@users.noreply.github.com>
2025-08-28 20:22:27 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // port
 
 app.get('/', (_, res) => {
     res.send('Hello World!');
