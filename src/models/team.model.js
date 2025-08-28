05422452ad86dd45cf75f0fc48b61473cf178f61
Vaibhavg987 <78644114+Vaibhavg987@users.noreply.github.com>
2025-08-28 20:25:07 +0530

    Initial empty commit

--- src/models/team.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const teamSchema = new mongoose.Schema({
-  name: String,
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-}, { timestamps: true });
-
-export const Team = mongoose.model("Team", teamSchema);
